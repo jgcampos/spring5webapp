598d26f17ea17a4cc6c92c52bed5c2b16a03aa93
jgcampos <alnilak@gmail.com>
2020-06-28 12:07:31 +0200

    Adding an authors thymeleaf template

---
+++ src/main/java/dev/jcampos/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package dev.jcampos.spring5webapp.controllers;
+
+import dev.jcampos.spring5webapp.model.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors",this.authorRepository.findAll());
+
+        return "authors/list";
+    }
+
+}
